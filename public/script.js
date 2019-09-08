bf8a2b989410466285d8f903a5b4b75a15096ca7
Nilay Savant <nilayofficial@gmail.com>
2019-09-09 01:37:09 +0530

    Add: functionality for multiple countdown cards on frontend

--- public/script.js
+++ public/script.js
@@ -8,31 +8,44 @@ let last_hit_date_array = document.getElementsByClassName("last-hit-date")
 let button_array = document.getElementsByClassName("card__button")
 
 /**
- * Run this on every page load so that inital data is set
+ * looping throught all countdown cards
+ * and updating them automatically on page load
  */
-postData(base_url + '/countdown', { countdown: false })
-    .then(data => {
-        // data: { count: 5, last_hit_datetime: "2019-09-08 19:36:59", total_countdowns: 6}
-
-        // Set the labels accordingly
-        count_array[0].innerHTML = parseInt(data.count)
-        total_countdowns_array[0].innerHTML = parseInt(data.total_countdowns)
-        last_hit_date_array[0].innerHTML = data.last_hit_datetime
-    })
-    .catch(error => console.error(error));
-
-// count down btn on click event
-button_array[0].onclick = function () {
-    postData(base_url + '/countdown', { countdown: true })
+for (let i = 0; i < count_array.length; i++) {
+    /**
+     * Run this on every page load so that inital data is set
+     */
+    postData(base_url + '/countdown', { countdown: false })
         .then(data => {
             // data: { count: 5, last_hit_datetime: "2019-09-08 19:36:59", total_countdowns: 6}
 
             // Set the labels accordingly
-            count_array[0].innerHTML = parseInt(data.count)
-            total_countdowns_array[0].innerHTML = parseInt(data.total_countdowns)
-            last_hit_date_array[0].innerHTML = data.last_hit_datetime
+            count_array[i].innerHTML = parseInt(data.count)
+            total_countdowns_array[i].innerHTML = parseInt(data.total_countdowns)
+            last_hit_date_array[i].innerHTML = data.last_hit_datetime
         })
         .catch(error => console.error(error));
+}
+
+/**
+ * looping throught all countdown cards
+ * and add the on click listener with functionality
+ * and data updates to each card
+ */
+for (let i = 0; i < count_array.length; i++) {
+    // count down btn on click event
+    button_array[i].onclick = function () {
+        postData(base_url + '/countdown', { countdown: true })
+            .then(data => {
+                // data: { count: 5, last_hit_datetime: "2019-09-08 19:36:59", total_countdowns: 6}
+
+                // Set the labels accordingly
+                count_array[i].innerHTML = parseInt(data.count)
+                total_countdowns_array[i].innerHTML = parseInt(data.total_countdowns)
+                last_hit_date_array[i].innerHTML = data.last_hit_datetime
+            })
+            .catch(error => console.error(error));
+    }
 }
 
 
