c7b9a27adb28dfec2878e8805e1399f65687c60e
Nilay Savant <nilayofficial@gmail.com>
2019-09-09 00:07:10 +0530

    ADD: frontent javscript, basic utn click tested.

---
+++ public/script.js
@@ -0,0 +1,9 @@
+
+let count_array = document.getElementsByClassName("card__count-number")
+let total_countdowns_array = document.getElementsByClassName("total-countdowns")
+let last_hit_date_array = document.getElementsByClassName("last-hit-date")
+let button_array = document.getElementsByClassName("card__button")
+
+button_array[0].onclick = function () {
+    console.log("clickkkk")
+}
